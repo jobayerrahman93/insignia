af89fdcd35266bb28b659f636ebb921751709c00
jobayercse71 <jobayercse71.com>
2021-12-17 23:00:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "0px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+  function responsiveSearch() {
+    document.getElementById("search-area").style.display = "block";
+    document.getElementById("responsive-search").style.display = "none";
+  }
+
+
+  // slideshow 
+
+  
+   
+  
